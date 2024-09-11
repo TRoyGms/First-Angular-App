2c639ce172f6c818d85c50a2e0118d531e43b4d6
Rodrigo Vz Rys (Roy) <152117781+TRoyGms@users.noreply.github.com>
2024-09-11 01:02:37 -0600

    First Commit

---
+++ src/app/users/user-form/user-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IUser } from '../modelo/iuser';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.css'
+})
+export class UserFormComponent {
+
+  @Input() user: IUser = {
+    id: 0,
+    name: "",
+    username: "",
+    phone: "",
+    website: ""
+  }
+
+  @Output() event = new EventEmitter<string>();
+
+  get message(): string {
+    return `Hola, Bienvenido ${this.user.username}`;
+  }
+
+  mostrar(): void {
+    console.log(this.user);
+    console.log(this.message);
+  }
+
+  enviar(): void {
+    this.event.emit(this.message);
+  }
+}
